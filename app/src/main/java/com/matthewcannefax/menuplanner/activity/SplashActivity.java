cbe4b94ca5165ab4618851f6b0f1277799a05403
Matthew Cannefax <mcannefax86@gmail.com>
2018-03-05 14:57:06 -0600

    Added a splash screen.

---
+++ app/src/main/java/com/matthewcannefax/menuplanner/activity/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.matthewcannefax.menuplanner.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
