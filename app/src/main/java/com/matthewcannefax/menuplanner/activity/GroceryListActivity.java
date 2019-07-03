531f45a6b5705bade6ca86b9fbfcf741f9e99174
Matthew Cannefax <mcannefax86@gmail.com>
2019-07-02 21:11:27 -0500

    added floating action button to grocery list activity in place of traditional button

--- app/src/main/java/com/matthewcannefax/menuplanner/activity/GroceryListActivity.java
+++ app/src/main/java/com/matthewcannefax/menuplanner/activity/GroceryListActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
@@ -78,14 +79,21 @@ public class GroceryListActivity extends AppCompatActivity {
         //initialize the listview
         //might change to recyclerview since it tends to be a little smoother while scrolling
         lv = findViewById(R.id.recipeMenuListView);
-        Button mButton = findViewById(R.id.addItemButton);
+//        Button mButton = findViewById(R.id.addItemButton);
+        FloatingActionButton fab = findViewById(R.id.fab);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addGroceryItem();
             }
         });
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                addGroceryItem();
+//            }
+//        });
 
         //this method to setup the grocery list adapter
         setGroceryListAdapter();
