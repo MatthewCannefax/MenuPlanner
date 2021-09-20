45ed0765752ac1b42545e139e2d9ef8374056169
mcann <mcannefax86@gmail.com>
2021-09-19 19:18:25 -0500

    singleActivity
    removed all admob stuff

--- app/src/androidTest/java/com/matthewcannefax/menuplanner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/matthewcannefax/menuplanner/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.matthewcannefax.menuplanner", appContext.getPackageName());
     }
